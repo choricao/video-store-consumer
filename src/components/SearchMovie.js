6226f2c9e604d79e5ace7c3d84606dd0a4c4250c
Alexandria Brown <axbn2190@gmail.com>
2018-06-19 11:09:32 -0700

    searchMovie component renders movies based on query

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,19 +1,75 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Movie from './Movie.js';
 
 const URL = "http://localhost:3001/movies"
 
-class SearchMovie extends Component {
+class SearchMovies extends Component {
+  constructor () {
+    super ();
+
+    this.state = {
+      title: "",
+      movies: [],
+    }
+  }
+
+  onFieldChange = (event) => {
+  const fieldName = event.target.name;
+  const fieldValue = event.target.value;
+  const updateState = {};
+
+  updateState[fieldName] = fieldValue;
+  this.setState(updateState);
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    console.log(URL + `?query=${this.state.title}`);
+    axios.get(URL + `?query=${this.state.title}`)
+    .then((response) => {
+      this.setState({
+        movies: response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  renderMovieList = () => {
+    const movieList = this.state.movies.map((movie) => {
+      return (
+        <Movie
+          key={movie.external_id}
+          title={movie.title}
+          releaseDate={movie.release_date}
+          imageURL={movie.image_url}
+          overview={movie.overview}
+        />
+      );
+    });
+    return movieList;
+  }
 
   render() {
     return (
-      <form>
-        <label/>
-        <input/>
-      </form>
+      <article>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="title" >Search: </label>
+          <input
+            name="title"
+            placeholder="movie title"
+            value={this.state.title}
+            onChange={this.onFieldChange}
+          />
+          <input type="submit" />
+        </form>
+        {this.renderMovieList()}
+      </article>
     )
   }
 
 }
 
-export default SearchMovie;
+export default SearchMovies;
